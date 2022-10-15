9df0898d0a9fc61ced44557bfdbcbfd79b4c1769
Isaac Harris <IsaacHphotography@gmail.com>
2022-10-15 11:53:18 -0600

    updated contact

---
+++ src/Components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className = "w-50 mx-auto">
+      <h1 className ="pb-4 pt-2">Contact Info</h1>
+      <p>
+      Contact me at the following
+
+      Email: IsaacHphotography@gmail.com
+
+      Gitgub: https://github.com/Hypnotoad0010 </p>
+    </div>
+  );
+}
