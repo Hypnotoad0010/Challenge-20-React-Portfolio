3a5f435ccf7ed15581380a9d83e18b58f68900ee
Isaac Harris <IsaacHphotography@gmail.com>
2022-10-15 11:14:58 -0600

    updated components js

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+<div class="card text-center fixed-bottom">
+  <div class="card-body d-flex justify-content-center">
+    <a href="https://github.com/Hypnotoad0010/" class="btn btn-primary btn-spacing">Github</a>
+    <a href="https://www.linkedin.com/in/all-glory-to-hypnotoad-28028a237/" class="btn btn-primary btn-spacing">Linkedin</a>
+  </div>
+</div>
+  );
+}
+
+
+export default Footer;
