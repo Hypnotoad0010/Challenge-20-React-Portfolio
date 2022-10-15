8e47b6e4d6c9994d493a6e2814b614a48859e6a2
Isaac Harris <IsaacHphotography@gmail.com>
2022-10-15 15:06:36 -0600

    updated linkedin

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 <div class="card text-center fixed-bottom">
   <div class="card-body d-flex justify-content-center">
     <a href="https://github.com/Hypnotoad0010/" class="btn btn-primary btn-spacing">Github</a>
-    <a href="https://www.linkedin.com/in/all-glory-to-hypnotoad-28028a237/" class="btn btn-primary btn-spacing">Linkedin</a>
+    <a href="https://www.linkedin.com/in/isaac-harris-28028a237/" class="btn btn-primary btn-spacing">Linkedin</a>
   </div>
 </div>
   );
