0b6ae8e67dc0ab21cd36b9baf641f6034b6eb744
Isaac Harris <IsaacHphotography@gmail.com>
2022-10-15 11:34:22 -0600

    updated resume and about me

---
+++ src/Components/pages/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div className = "w-50 mx-auto">
+      <h1 className ="pb-4 pt-2">Resume</h1>
+      <p>
+        Updated Resume coming soon!
+        </p>
+    </div>
+  );
+}
